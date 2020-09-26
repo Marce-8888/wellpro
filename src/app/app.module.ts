ed479b2a7cb6d3171dd8191348191ec057b51375
pamelanancupil <pamela.nancupilv@gmail.com>
2020-09-26 18:56:51 -0300

    Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { FeedComponent } from './components/feed/feed.component';
+import { HelpComponent } from './components/help/help.component';
+import { StartComponent } from './components/start/start.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    PerfilComponent,
+    TodoComponent,
+    FeedComponent,
+    HelpComponent,
+    StartComponent
   ],
   imports: [
     BrowserModule
