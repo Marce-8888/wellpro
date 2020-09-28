79c6f5137e70ce109075b0136779f23dceb6a2ce
AleKristen <correo@gmail.com>
2020-09-28 11:26:45 -0300

    intento ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  inf: any;
+  info: Array<{}> = [
+    {
+      "img":1,
+      "text":"Realizar la tarea dentro del horario de trabajo"
+    },
+    {
+      "img":2,
+      "text":"Responder las estrategias que usaste en el chech out"
+    },
+    {
+      "img":3,
+      "text":"Mantener una conducta de autocuidado"
+    },
+    {
+      "img":4,
+      "text":"Para ganar el premio, deben completar el desafío dentro del tiempo. Sino, igualmente deben completar la tarea, pero no habrá premio."
+    },
+  ]
+
+
+
   constructor() { }
 
   ngOnInit(): void {
