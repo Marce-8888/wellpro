ef6849091a2a8b58a1c89b35f782fdf32ccfb7b4
AleKristen <correo@gmail.com>
2020-09-29 17:00:56 -0300

    add componente leader-home

---
+++ src/app/components/leader-home/leader-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-leader-home',
+  templateUrl: './leader-home.component.html',
+  styleUrls: ['./leader-home.component.css']
+})
+export class LeaderHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
