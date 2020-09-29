f0357c2bbe9e4c6df0c8c4ea42fa015f526e2d15
pamelanancupil <pamela.nancupilv@gmail.com>
2020-09-28 21:36:43 -0300

    Perfil ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -8,6 +10,8 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
 import { TodoComponent } from './components/todo/todo.component';
+import { from } from 'rxjs';
+
 
 const route: Routes = [
   {
@@ -45,6 +49,8 @@ const route: Routes = [
   imports: [
     BrowserModule,
     RouterModule.forRoot(route),
+    BrowserAnimationsModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
