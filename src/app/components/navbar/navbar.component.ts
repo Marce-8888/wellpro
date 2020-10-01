02be156b565dc98e8dabf206f1235d31b9a26272
pamelanancupil <pamela.nancupilv@gmail.com>
2020-10-01 00:07:05 -0300

    Modal checkout

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  showCheckout:boolean=false;
+
+  info: any;
+  infoCheckout: Array<{}> = [
+    {
+      text:'Me conecté temprano',
+    },
+    {
+      text:'Planifiqué mi semana el día Lunes'
+    },
+    {
+      text:'Fui planificando día a día'
+    },
+    {
+      text:'Pedí ayuda a mis compañer@s'
+    },
+    {
+      text:'Me concentré toda la jornada'
+    },
+    {
+      text:'Respeté mis horarios de trabajo'
+    },
+    {
+      text:'Hice pausas de descanso'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
